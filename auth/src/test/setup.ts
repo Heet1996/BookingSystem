8548872dff89b4085478ec607a9b559373b65145
Heet1996 <heet1885@gmail.com>
2020-10-23 23:01:25 +0530

    Added SignUp Test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -2,14 +2,27 @@ import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import {app} from '../app';
 
+let mongo: any;
 beforeAll(async ()=>{
-    const mongo= new MongoMemoryServer();
+    process.env.JWT_KEY='assadsdkasd';
+    mongo= new MongoMemoryServer();
     const mongoUri= await mongo.getUri();
 
     await mongoose.connect(mongoUri, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
+});
 
-    
+beforeEach(async ()=>{
+     
+    const collections= await mongoose.connection.db.collections();
+    for(let collection of collections){
+        await collection.deleteMany({});
+    }
+})
+
+afterAll(async ()=>{
+    mongo.stop();
+    await mongoose.disconnect();
 })
